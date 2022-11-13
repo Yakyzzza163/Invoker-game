ab70b7272019985602cdf8016e29e0aca59eb28b
yakyzzza163 <ksport124@gmail.com>
2022-11-13 19:31:45 +0700

    add

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ const Buttons = {
 	spheres: [...document.querySelectorAll(".button-sphere")],
 	skills: [...document.querySelectorAll(".button-skill")]
   }
-
 Buttons.spheres.at(0).onclick = function(clickEvent){
 console.log (clickEvent.type)
 	
@@ -17,12 +16,22 @@ Buttons.spheres.at(2).onclick = function(clickEvent){
 
 }
 
+Buttons.skills.at(0).onclick = function(clickEvent){
+
+}
+
+Buttons.skills.at(1).onclick = function(clickEvent){
+
+}
+
 document.addEventListener("keydown", ({key}) => {
 	
 	const table = {
 		"q": Buttons.spheres.at(0),
 		"w": Buttons.spheres.at(1),
-		"e": Buttons.spheres.at(2)
+		"e": Buttons.spheres.at(2),
+		"d": Buttons.skills.at(0),
+		"f": Buttons.skills.at(1)
 	};
 
 	if (key in table === false){
@@ -33,3 +42,25 @@ document.addEventListener("keydown", ({key}) => {
 })
 
 console.log (Buttons.skills)
+
+const Invoker = {
+	casted: [],
+	castThreeSphere: (Content) => {
+		Invoker.casted.length = 0;
+		const skillsTable = {"qqq": "cold snap"}
+		
+
+
+	},
+	castSphere: (content) => {
+	   Invoker.casted.push(content);
+	   if (Invoker.casted.length === 3){
+		Invoker.castThreeSphere()
+	   }
+	}
+  }
+  
+  Buttons.spheres.forEach(node => {
+  const content = node.textContent;
+  node.onclick = Invoker.castSphere.bind(null, content);
+  }); 
