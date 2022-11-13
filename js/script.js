6a2425e6a3c34ff5d4f18a0daf5bdd0dbdad7ae0
Alexsandr1x <zoodogood@gmail.com>
2022-11-13 10:41:53 +0200

    Add keyevent

--- js/script.js
+++ js/script.js
@@ -12,3 +12,18 @@ buttons.at(1).onclick = function(){
 buttons.at(2).onclick = function(){
 
 }
+
+document.addEventListener("keydown", ({key}) => {
+	
+	const table = {
+		"q": buttons.at(0),
+		"w": buttons.at(0),
+		"e": buttons.at(0)
+	};
+
+	if (key in table === false){
+		return;
+	}
+
+	table[key].click();
+})
